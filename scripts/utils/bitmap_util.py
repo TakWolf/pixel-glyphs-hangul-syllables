0371429de4a0862266d19f91ea69934a1dd7d7fe
TakWolf <takwolf@foxmail.com>
2024-05-20 17:14:46 +0800

    代码整理

--- scripts/utils/bitmap_util.py
+++ scripts/utils/bitmap_util.py
@@ -5,33 +5,27 @@ import png
 
 def load_png(file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes]) -> tuple[list[list[int]], int, int]:
     width, height, pixels, _ = png.Reader(filename=file_path).read()
-    data = []
+    bitmap = []
     for pixels_row in pixels:
-        data_row = []
-        for x in range(0, width * 4, 4):
-            alpha = pixels_row[x + 3]
-            if alpha > 127:
-                data_row.append(1)
-            else:
-                data_row.append(0)
-        data.append(data_row)
-    return data, width, height
+        bitmap_row = []
+        for i in range(0, width * 4, 4):
+            alpha = pixels_row[i + 3]
+            bitmap_row.append(1 if alpha > 127 else 0)
+        bitmap.append(bitmap_row)
+    return bitmap, width, height
 
 
 def save_png(
-        data: list[list[int]],
+        bitmap: list[list[int]],
         file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes],
 ):
     pixels = []
-    for data_row in data:
+    for bitmap_row in bitmap:
         pixels_row = []
-        for x in data_row:
+        for alpha in bitmap_row:
             pixels_row.append(0)
             pixels_row.append(0)
             pixels_row.append(0)
-            if x == 0:
-                pixels_row.append(0)
-            else:
-                pixels_row.append(255)
+            pixels_row.append(255 if alpha != 0 else 0)
         pixels.append(pixels_row)
     png.from_array(pixels, 'RGBA').save(file_path)
