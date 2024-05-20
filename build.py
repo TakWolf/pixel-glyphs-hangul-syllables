5ca7dbd99e7370e2ca4cd1b225414b6d4f27be2d
TakWolf <takwolf@foxmail.com>
2024-05-20 17:09:27 +0800

    添加 build 脚本

--- build.py
+++ build.py
@@ -1,6 +1,18 @@
+from scripts import configs
+from scripts.configs import path_define
+from scripts.services import glyph_service, publish_service
+from scripts.utils import fs_util
+
 
 def main():
-    pass
+    fs_util.delete_dir(path_define.outputs_dir)
+    fs_util.delete_dir(path_define.releases_dir)
+
+    for font_size in configs.font_sizes:
+
+        # TODO
+
+        publish_service.make_release_zip(font_size)
 
 
 if __name__ == '__main__':
