202dc51185eb0cba5ebeca473542a7168ba2b8bd
TakWolf <takwolf@foxmail.com>
2024-05-22 17:39:12 +0800

    简化 fs_util

--- scripts/services/publish_service.py
+++ scripts/services/publish_service.py
@@ -4,13 +4,12 @@ import zipfile
 
 from scripts import configs
 from scripts.configs import path_define
-from scripts.utils import fs_util
 
 logger = logging.getLogger('publish_service')
 
 
 def make_release_zip(font_size: int):
-    fs_util.make_dir(path_define.releases_dir)
+    os.makedirs(path_define.releases_dir, exist_ok=True)
     zip_file_path = os.path.join(path_define.releases_dir, f'pixel-glyphs-hangul-syllables-{font_size}px-v{configs.version}.zip')
     with zipfile.ZipFile(zip_file_path, 'w') as file:
         outputs_dir = os.path.join(path_define.outputs_dir, str(font_size))
