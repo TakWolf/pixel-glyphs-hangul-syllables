a6221417255c0966af500b369168b668bc435ee7
TakWolf <takwolf@foxmail.com>
2024-06-07 03:14:01 +0800

    Release 包添加许可证

--- scripts/services/publish_service.py
+++ scripts/services/publish_service.py
@@ -11,6 +11,7 @@ def make_release_zip(font_size: int):
     path_define.releases_dir.mkdir(parents=True, exist_ok=True)
     zip_file_path = path_define.releases_dir.joinpath(f'pixel-glyphs-hangul-syllables-{font_size}px-v{configs.version}.zip')
     with zipfile.ZipFile(zip_file_path, 'w') as file:
+        file.write(path_define.project_root_dir.joinpath('LICENSE-OFL'), 'OFL.txt')
         outputs_dir = path_define.outputs_dir.joinpath(str(font_size))
         for file_dir, _, file_names in outputs_dir.walk():
             for file_name in file_names:
