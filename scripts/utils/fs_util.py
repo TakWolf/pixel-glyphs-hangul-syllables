35f0179a62eb6bcfa1362d00fbecb11e809df1a0
TakWolf <takwolf@foxmail.com>
2024-06-07 10:04:53 +0800

    清理 fs_util

--- scripts/utils/fs_util.py
+++ scripts/utils/fs_util.py
@@ -1,13 +1,7 @@
 import shutil
-import tomllib
 from pathlib import Path
-from typing import Any
 
 
 def delete_dir(path: Path):
     if path.exists():
         shutil.rmtree(path)
-
-
-def read_toml(path: Path) -> Any:
-    return tomllib.loads(path.read_text('utf-8'))
