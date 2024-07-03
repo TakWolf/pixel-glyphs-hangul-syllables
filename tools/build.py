22fa284fd96f6db8b5fd7fc75cffc1d80976e59e
TakWolf <takwolf@foxmail.com>
2024-07-03 17:34:56 +0800

    删除 fs_util.delete_dir()

--- tools/build.py
+++ tools/build.py
@@ -1,12 +1,13 @@
+import shutil
+
 from tools import configs
 from tools.configs import path_define
 from tools.services import publish_service
-from tools.utils import fs_util
 
 
 def main():
-    fs_util.delete_dir(path_define.outputs_dir)
-    fs_util.delete_dir(path_define.releases_dir)
+    if path_define.build_dir.exists():
+        shutil.rmtree(path_define.build_dir)
 
     for font_size in configs.font_sizes:
 
--- tools/utils/fs_util.py
+++
@@ -1,7 +0,0 @@
-import shutil
-from pathlib import Path
-
-
-def delete_dir(path: Path):
-    if path.exists():
-        shutil.rmtree(path)
