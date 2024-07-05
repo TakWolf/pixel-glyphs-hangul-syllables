426101b4b53994ded89d74e1e6d4545752086ab4
TakWolf <takwolf@foxmail.com>
2024-07-05 11:34:54 +0800

    代码整理

--- tools/build.py
+++ tools/build.py
@@ -2,7 +2,7 @@ import shutil
 
 from tools import configs
 from tools.configs import path_define
-from tools.services import publish_service
+from tools.services import glyph_service, publish_service
 
 
 def main():
@@ -10,9 +10,7 @@ def main():
         shutil.rmtree(path_define.build_dir)
 
     for font_size in configs.font_sizes:
-
-        # TODO
-
+        glyph_service.make_patterns(font_size)
         publish_service.make_release_zip(font_size)
 
 
---
+++ tools/services/glyph_service.py
@@ -0,0 +1,4 @@
+
+def make_patterns(font_size: int):
+    # TODO
+    pass
