22d614a553e257f8fd89eb353e4b90174ee2a691
TakWolf <takwolf@foxmail.com>
2024-07-18 11:32:10 +0800

    设置输出目录

--- tools/services/glyph_service.py
+++ tools/services/glyph_service.py
@@ -2,5 +2,5 @@ from tools.configs import path_define, FontSize
 
 
 def make_patterns(font_size: FontSize):
-    # TODO
-    pass
+    outputs_dir = path_define.outputs_dir.joinpath(str(font_size), 'AC00-D7AF Hangul Syllables')
+    outputs_dir.mkdir(parents=True, exist_ok=True)
