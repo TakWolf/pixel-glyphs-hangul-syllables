b09b1c1c089a6099f6ba57a2d03dc278e6c0e687
TakWolf <takwolf@foxmail.com>
2026-09-18 15:03:19 +0800

    修复构建

--- tools/services/glyph_service.py
+++ tools/services/glyph_service.py
@@ -1,5 +1,5 @@
 from loguru import logger
-from pixel_font_knife.mono_bitmap import MonoBitmap
+from pixel_font_knife.bitmap.mono_bitmap import MonoBitmap
 
 from tools.configs import path_define, FontSize
 from tools.utils import hangul_util
@@ -10,6 +10,6 @@ def make_glyphs(font_size: FontSize) -> None:
     outputs_dir.mkdir(parents=True, exist_ok=True)
 
     for syllable in hangul_util.iter_syllables():
-        canvas = MonoBitmap.create(font_size, font_size)
+        canvas = MonoBitmap.blank(font_size, font_size)
         canvas.save_png(outputs_dir.joinpath(f'{syllable.code_point:04X}.png'))
     logger.info("Make glyphs: {}px", font_size)
