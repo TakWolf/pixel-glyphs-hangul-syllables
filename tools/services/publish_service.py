212a67d000d6ad556048bf712b29548adf84e964
TakWolf <takwolf@foxmail.com>
2026-09-22 17:47:58 +0800

    优化在目录中遍历文件的实现

--- tools/services/publish_service.py
+++ tools/services/publish_service.py
@@ -15,11 +15,8 @@ def make_release_zip(font_size: FontSize) -> None:
         file.write(path_define.PROJECT_ROOT_DIR.joinpath('LICENSE-OFL'), 'OFL.txt')
 
         outputs_dir = path_define.OUTPUTS_DIR.joinpath(str(font_size))
-        for file_dir, _, file_names in outputs_dir.walk():
-            for file_name in file_names:
-                if not file_name.endswith('.png'):
-                    continue
-
-                file_path = file_dir.joinpath(file_name)
-                file.write(file_path, file_path.relative_to(outputs_dir))
+        for file_path in sorted(outputs_dir.rglob('*.png')):
+            if not file_path.is_file():
+                continue
+            file.write(file_path, file_path.relative_to(outputs_dir))
     logger.info("Make release zip: '{}'", zip_file_path)
